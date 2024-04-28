a75e894879567f484fdad2df27b8d9a5754ed3bc
Ishrak Hamim Mahi <134547296+IshrakHamim@users.noreply.github.com>
2024-04-28 14:34:48 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,12 +61,13 @@ def cgcon(cg):
 
     
 def main():
+    st.title('FutureFit')
     st.title('Job Salary Prediction Web App') 
     
     dsa = st.text_input('How many DSA Questions have you solved so far?')
     cgpa = st.text_input('What is your CGPA?')
-    ml = st.selectbox(label = 'Do you know ML?', options = ["Yes", "No"])
-    kdsa = st.selectbox(label = 'Do you know DSA?', options = ["Yes", "No"])
+    ml = st.selectbox(label = 'Do you know Machine Learning?', options = ["Yes", "No"])
+    kdsa = st.selectbox(label = 'Do you know Data Structures Algorithms?', options = ["Yes", "No"])
     python = st.selectbox(label = 'Do you know Python?', options = ["Yes", "No"])
     js = st.selectbox(label = 'Do you know JavaScript?', options = ["Yes", "No"])
     html = st.selectbox(label = 'Do you know HTML?', options = ["Yes", "No"])
